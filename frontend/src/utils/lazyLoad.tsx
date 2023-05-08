f376a28824b95bb82f8a656257488a232982297c
Marcu Operario <marcuoperario@gmail.com>
2023-05-08 16:14:28 +0800

    feat: update layout structure, spinner color, and spinner size.

--- frontend/src/utils/lazyLoad.tsx
+++ frontend/src/utils/lazyLoad.tsx
@@ -1,14 +1,21 @@
+import { Flex } from '@chakra-ui/react';
 import { lazy, Suspense } from 'react';
-import { CubeSpinner } from 'react-spinners-kit';
+import { MagicSpinner } from 'react-spinners-kit';
 
 const lazyLoad = (importFunc: () => Promise<any>) => {
     const Loading = () => {
         return (
-            <section className = "min-h-screen bg-[#141517]">
-                <div className = "flex items-center justify-center min-h-screen">
-                    <CubeSpinner size = { 42 } frontColor = "#E6613E" backColor = "#b95238" loading />
-                </div>
-            </section>
+            <Flex
+                minH = "100vh"
+                align = "center"
+                justify = "center"
+            >
+                <MagicSpinner 
+                    size = { 70 }
+                    color = "#3a82f7"
+                    loading 
+                />
+            </Flex>
         )
     };
     
