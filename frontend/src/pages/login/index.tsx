958cfe5a895de477148c10b728c73994b4e8e2ed
Marcu Operario <marcuoperario@gmail.com>
2023-05-08 15:35:04 +0800

    feat: update Login page layout

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import AuthLayout from '../../components/auth/authlayout';
+import LoginForm from '../../components/auth/login/loginform';
+import AuthCardFooter from '../../components/auth/authlayout/authcardfooter';
 
 const Login = () => {
   return (
-    <div>
-        Login
-    </div>
+    <AuthLayout 
+      heading = "Log in to your account"
+      formComponent = {
+        <LoginForm />
+      }
+      footer = {
+        <AuthCardFooter 
+          message = "Don't have an account?"
+          navigateText = "Register"
+          navigateLink = "/register"
+        />
+      }
+    />
   )
 }
 
