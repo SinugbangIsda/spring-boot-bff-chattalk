480b3641e5977ab0fb523170a444e7c3739c3103
Marcu Operario <marcuoperario@gmail.com>
2023-05-09 09:25:43 +0800

    feat: add Chat page

---
+++ frontend/src/pages/chat/index.tsx
@@ -0,0 +1,11 @@
+import Applayout from '../../components/privateroute/applayout';
+
+const Chat = () => {
+  return (
+    <Applayout>
+        <h1>Chat</h1>
+    </Applayout>
+  )
+}
+
+export default Chat;
