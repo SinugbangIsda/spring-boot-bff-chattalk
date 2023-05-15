b673c6e20b37d768c647e750ee5bb521221a14a0
Marcu Operario <marcuoperario@gmail.com>
2023-05-15 11:04:53 +0800

    feat: add props interface and update UI layout for AppLayout

--- frontend/src/components/privateroute/applayout/index.tsx
+++ frontend/src/components/privateroute/applayout/index.tsx
@@ -1,70 +1,68 @@
-import { Flex, Box, Drawer, DrawerContent, DrawerOverlay, Icon, IconButton, useDisclosure, Stack } from "@chakra-ui/react";
+import { Flex, Box, Drawer, DrawerContent, DrawerOverlay, IconButton, useDisclosure, Stack } from "@chakra-ui/react";
 import SidebarContent from "./sidebar";
 import { FiMenu } from "react-icons/fi";
-import { FaBell } from "react-icons/fa";
+import { AppLayoutProps } from "../../../interfaces";
 
-const Applayout = ({ children }: any) => {
+const Applayout = ({ children }: AppLayoutProps) => {
   const { isOpen, onClose, onOpen } = useDisclosure();
   return (
-      <Box as = "section" minH = "100vh">
-        <SidebarContent display = {{ base: "none", md: "unset"}} />
-        <Drawer
-          isOpen = { isOpen }
-          onClose = { onClose }
-          placement = "left"
-        >
-          <DrawerOverlay />
-          <DrawerContent>
-            <SidebarContent
-              w = "full"
-              borderRight = "none"
-            />
-          </DrawerContent>
-        </Drawer>
-        <Box
-          ml = {{ base: 0, md: 64 }}
-          transition = ".3s ease"
-        >
-          <Flex
-            as = "header"
-            align = "center"
-            justify = {{ base: "space-between", md: "flex-end"}}
+    <Box 
+      as = "section" 
+      minH = "100vh"
+      overflowY = "hidden"
+    >
+      <SidebarContent display = {{ base: "none", md: "unset"}} />
+      <Drawer
+        isOpen = { isOpen }
+        onClose = { onClose }
+        placement = "left"
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <SidebarContent
             w = "full"
-            px = "4"
-            borderBottomWidth = "1px"
-            borderColor = "inherit"
-            bg = "white"
-            boxShadow = "sm"
-            h = "14"
-          >
-            <IconButton
-              aria-label = "Menu"
-              display = {{ base: "inline-flex", md: "none"}}
-              onClick = { onOpen }
-              icon = { 
-                <FiMenu />
-              }
-              size = "md"
-            />
-            <Flex align = "center">
-              <Icon
-                color = "gray.500"
-                as = { FaBell }
-                cursor = "pointer"
-              />
-          </Flex>
+            borderRight = "none"
+          />
+        </DrawerContent>
+      </Drawer>
+      <Box
+        ml = {{ base: 0, md: 64 }}
+        transition = ".3s ease"
+      >
+        <Flex
+          as = "header"
+          align = "center"
+          justify = {{ base: "space-between", md: "flex-end"}}
+          w = "full"
+          px = "4"
+          borderBottomWidth = "1px"
+          borderColor = "inherit"
+          bg = "white"
+          boxShadow = "sm"
+          h = "14"
+        >
+          <IconButton
+            aria-label = "Menu"
+            display = {{ base: "inline-flex", md: "none"}}
+            onClick = { onOpen }
+            icon = { 
+              <FiMenu />
+            }
+            size = "md"
+          />
         </Flex>
         <Box
           as = "main"
           p = "4"
-          minH = "25rem"
+          h = "full"
           bg = "auto"
         >
           <Stack
             direction = {{ base: "column", md: "row" }}
             alignItems = "center"
             justifyContent = "center"
-            h = "100%"
+            h = "full"
+            overflowY = "auto"
           >
             <Stack spacing = "4">
               { children }
