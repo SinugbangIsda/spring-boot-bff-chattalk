ebb933783bcebabab7541f1547d88718a1a67c40
breadmyburn <18ang.mss@gmail.com>
2023-05-15 13:29:23 +0800

    build: initial commit

---
+++ backend/chattalk/chattalk-provider/src/test/java/com/sunognaisda/chattalkprovider/ChattalkProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunognaisda.chattalkprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattalkProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
