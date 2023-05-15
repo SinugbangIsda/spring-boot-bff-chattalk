42913fc2e4f63e474f6c5b106ceff7fb50ff4a8f
Marcu Operario <marcuoperario@gmail.com>
2023-05-15 09:12:49 +0800

    feat: add search input component

---
+++ frontend/src/components/privateroute/applayout/sidebar/search.tsx
@@ -0,0 +1,45 @@
+import { FormControl, Input, InputGroup, InputRightElement } from "@chakra-ui/react";
+import { IoSearch } from "react-icons/all";
+
+const SearchInput = ({ onChange, onClick, value, ref }: any ) => {
+    return (
+        <FormControl 
+            id = "search" 
+            color = "gray.500"
+            mr = {{ base: 0, sm: 4 }}
+            mb = {{ base: 4, sm: 0 }}
+            my = "1"
+        >
+            <InputGroup width = {{ base: "full" }}>
+                <Input 
+                    placeholder = "Search"
+                    type = "text"
+                    bg = "gray.100"
+                    color = "black"
+                    _placeholder = {{
+                        color: "gray.400"
+                    }}
+                    border = "1px"
+                    borderColor = "gray.100"
+                    rounded = "md"
+                    value = { value }
+                    ref = { ref }
+                    autoComplete = "off"
+                    onChange = { onChange }
+                />
+                <InputRightElement
+                    css = {{
+                        "WebkitUserSelect": "none",
+                        "msUserSelect": "none",
+                        "userSelectg": "none",
+                    }}
+                    onClick = { onClick }
+                >
+                    <IoSearch />
+                </InputRightElement>
+            </InputGroup>
+        </FormControl>
+    )
+};
+
+export default SearchInput;
