4e87f6c94375ea80eb3e73b107f61ff81b765286
Marcu Operario <marcuoperario@gmail.com>
2023-05-18 10:12:56 +0800

    refactor: restructure AppyLayout component

--- frontend/src/components/privateroute/applayout/index.tsx
+++ frontend/src/components/privateroute/applayout/index.tsx
@@ -1,77 +1,97 @@
-import { Flex, Box, Drawer, DrawerContent, DrawerOverlay, IconButton, useDisclosure, Stack } from "@chakra-ui/react";
-import SidebarContent from "./sidebar";
-import { FiMenu } from "react-icons/fi";
-import { AppLayoutProps } from "../../../interfaces";
+// import { Flex, Box, Drawer, DrawerContent, DrawerOverlay, IconButton, useDisclosure, Stack } from "@chakra-ui/react";
+// import SidebarContent from "./sidebar";
+// import { FiMenu } from "react-icons/fi";
+// import { AppLayoutProps } from "../../../interfaces";
 
-const Applayout = ({ children }: AppLayoutProps) => {
-  const { isOpen, onClose, onOpen } = useDisclosure();
+// const Applayout = ({ children }: AppLayoutProps) => {
+//   const { isOpen, onClose, onOpen } = useDisclosure();
+//   return (
+//     <Box 
+//       as = "section" 
+//       minH = "100vh"
+//       overflowY = "hidden"
+//     >
+//       <SidebarContent display = {{ base: "none", md: "unset"}} />
+//       <Drawer
+//         isOpen = { isOpen }
+//         onClose = { onClose }
+//         placement = "left"
+//       >
+//         <DrawerOverlay />
+//         <DrawerContent>
+//           <SidebarContent
+//             w = "full"
+//             borderRight = "none"
+//           />
+//         </DrawerContent>
+//       </Drawer>
+//       <Box
+//         ml = {{ base: 0, md: 64 }}
+//         transition = ".3s ease"
+//       >
+//         <Flex
+//           as = "header"
+//           align = "center"
+//           justify = {{ base: "space-between", md: "flex-end"}}
+//           w = "full"
+//           px = "4"
+//           borderBottomWidth = "1px"
+//           borderColor = "inherit"
+//           bg = "white"
+//           boxShadow = "sm"
+//           h = "14"
+//         >
+//           <IconButton
+//             aria-label = "Menu"
+//             display = {{ base: "inline-flex", md: "none"}}
+//             onClick = { onOpen }
+//             icon = { 
+//               <FiMenu />
+//             }
+//             size = "md"
+//           />
+//         </Flex>
+//         <Box
+//           as = "main"
+//           p = "4"
+//           h = "full"
+//           bg = "auto"
+//         >
+//           <Stack
+//             direction = {{ base: "column", md: "row" }}
+//             alignItems = "center"
+//             justifyContent = "center"
+//             h = "full"
+//             overflowY = "auto"
+//           >
+//             <Stack spacing = "4">
+//               { children }
+//             </Stack>
+//           </Stack>
+//         </Box>
+//       </Box>
+//     </Box>
+//   )
+// }
+
+// export default Applayout;
+
+import { Box } from "@chakra-ui/react";
+import { AppLayoutProps } from "../../../interfaces";
+import Sidebar from "./sidebar";
+
+const AppLayout = ({ children }: AppLayoutProps) => {
   return (
-    <Box 
-      as = "section" 
-      minH = "100vh"
-      overflowY = "hidden"
+    <Box
+      display = {{ base: "none", lg: "block" }}
+      pl = {{ lg: "72" }}
+      h = "100vh"
     >
-      <SidebarContent display = {{ base: "none", md: "unset"}} />
-      <Drawer
-        isOpen = { isOpen }
-        onClose = { onClose }
-        placement = "left"
-      >
-        <DrawerOverlay />
-        <DrawerContent>
-          <SidebarContent
-            w = "full"
-            borderRight = "none"
-          />
-        </DrawerContent>
-      </Drawer>
-      <Box
-        ml = {{ base: 0, md: 64 }}
-        transition = ".3s ease"
-      >
-        <Flex
-          as = "header"
-          align = "center"
-          justify = {{ base: "space-between", md: "flex-end"}}
-          w = "full"
-          px = "4"
-          borderBottomWidth = "1px"
-          borderColor = "inherit"
-          bg = "white"
-          boxShadow = "sm"
-          h = "14"
-        >
-          <IconButton
-            aria-label = "Menu"
-            display = {{ base: "inline-flex", md: "none"}}
-            onClick = { onOpen }
-            icon = { 
-              <FiMenu />
-            }
-            size = "md"
-          />
-        </Flex>
-        <Box
-          as = "main"
-          p = "4"
-          h = "full"
-          bg = "auto"
-        >
-          <Stack
-            direction = {{ base: "column", md: "row" }}
-            alignItems = "center"
-            justifyContent = "center"
-            h = "full"
-            overflowY = "auto"
-          >
-            <Stack spacing = "4">
-              { children }
-            </Stack>
-          </Stack>
-        </Box>
-      </Box>
+      <Sidebar>
+        { children }
+      </Sidebar>
     </Box>
   )
-}
+};
 
-export default Applayout;
+export default AppLayout;
