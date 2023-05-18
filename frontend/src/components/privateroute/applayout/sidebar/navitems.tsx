239b3f09ccbf90b9154cbbd9dba9f33a1fd817af
Marcu Operario <marcuoperario@gmail.com>
2023-05-18 10:11:33 +0800

    feat: add NavItems

---
+++ frontend/src/components/privateroute/applayout/sidebar/navitems.tsx
@@ -0,0 +1,16 @@
+import { IoChatbubbleEllipses, TbUser } from "react-icons/all";
+
+const NavItems = [
+    {
+        label: "Chat",
+        icon: IoChatbubbleEllipses,
+        url: "/chat"
+    },
+    {
+        label: "Users",
+        icon: TbUser,
+        url: "/users"
+    }
+];
+
+export default NavItems;
