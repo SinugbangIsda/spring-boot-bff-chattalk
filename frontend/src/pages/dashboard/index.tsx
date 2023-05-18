52bf409b4547eece1a650e0148fc8ce0e63d17b4
Marcu Operario <marcuoperario@gmail.com>
2023-05-18 10:09:12 +0800

    refactor: restructure Dashboard page

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,26 +1,36 @@
-import { VStack, Text } from "@chakra-ui/react";
+import { VStack, Text, Box } from "@chakra-ui/react";
 import Applayout from "../../components/privateroute/applayout";
 import { IoLogoWechat } from "react-icons/all";
 
 const Dashboard = () => {
   return (
     <Applayout>
-      <VStack>
-        <Text
-          color = "gray.500"
-          fontSize = "7xl"
-          textAlign = "center"
-        >
-          <IoLogoWechat />
-        </Text>
-        <Text
-          color = "gray.500"
-          fontSize = "xl"
-          textAlign = "center"
-        >
-          Choose a conversation to participate in or initiate a fresh one.
-        </Text>
-      </VStack>
+      <Box
+        py = "10"
+        px = {{ sm: "6", lg: "8"}}
+        h = "full"
+        display = "flex"
+        justifyContent = "center"
+        alignItems = "center"
+        bg = "gray.50"
+      >
+        <VStack>
+          <Text
+            color = "gray.500"
+            fontSize = "7xl"
+            textAlign = "center"
+          >
+            <IoLogoWechat />
+          </Text>
+          <Text
+            color = "gray.500"
+            fontSize = "xl"
+            textAlign = "center"
+          >
+            Choose a conversation to participate in or initiate a fresh one.
+          </Text>
+        </VStack>
+      </Box>
     </Applayout>
   )
 }
