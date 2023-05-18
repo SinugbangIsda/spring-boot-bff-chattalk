efb64ca6959b108b9db00b2237232a12d500c526
Marcu Operario <marcuoperario@gmail.com>
2023-05-18 10:15:05 +0800

    refactor: restructure Chat page

--- frontend/src/pages/chat/index.tsx
+++ frontend/src/pages/chat/index.tsx
@@ -1,9 +1,20 @@
+import { Box } from '@chakra-ui/react';
 import Applayout from '../../components/privateroute/applayout';
 
 const Chat = () => {
   return (
     <Applayout>
-        <h1>Chat</h1>
+      <Box
+        py = "10"
+        px = {{ sm: "6", lg: "8"}}
+        h = "full"
+        display = "flex"
+        justifyContent = "center"
+        alignItems = "center"
+        bg = "gray.50"
+      >
+        Chat page
+      </Box>
     </Applayout>
   )
 }
